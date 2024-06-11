6c92b05fe85e6b02dca0a09e86d1321402bb10d9
Bome9 <developdr3am@gmail.com>
2024-06-11 20:06:12 +0300

    changes1

--- src/App.js
+++ src/App.js
@@ -8,14 +8,9 @@ function App() {
         tg.ready();
     }, [])
 
-    const onClose = () => {
-        tg.close();
-    }
-
     return (
         <div className="App">
             <h1>Hello World</h1>
-            <button onClick={onClose}>Close</button>
         </div>
     );
 }
